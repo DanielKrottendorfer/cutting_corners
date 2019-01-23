3832e019c09c7ad1437cc8e9a72124ee76fd9f29
eraiser <krotttendorfer@hotmail.de>
2019-01-24 00:10:19 +0100

    cleanup

--- src/game/drawable/mesh.rs
+++ src/game/drawable/mesh.rs
@@ -1,18 +1,16 @@
-
 #[derive(Copy, Clone)]
 pub struct Vert {
-    position: [f32;3]
+    position: [f32; 3],
 }
 
 glium::implement_vertex!(Vert, position);
 
 #[derive(Copy, Clone)]
 pub struct Norm {
-    normal: [f32;3]
+    normal: [f32; 3],
 }
 
 implement_vertex!(Norm, normal);
-
 
 pub struct StaticMesh {
     pub vertices: glium::VertexBuffer<Vert>,
@@ -25,41 +23,44 @@ pub struct StaticMesh {
 }
 
 impl StaticMesh {
-    pub fn load_obj(display:&mut glium::Display, path: &str) -> StaticMesh {
-
+    pub fn load_obj(display: &mut glium::Display, path: &str) -> StaticMesh {
+        use obj::*;
         use std::fs::File;
         use std::io::BufReader;
-        use obj::*;
 
-        println!("{}",path);
+        println!("{}", path);
         let input = BufReader::new(File::open(path).unwrap());
         let demo: Obj = obj::load_obj(input).unwrap();
         let vertecis = demo.vertices;
-        let indices:Vec<u16>  = demo.indices;
+        let indices: Vec<u16> = demo.indices;
 
-        let mut positions:Vec<Vert> = Vec::new();
-        let mut normals:Vec<Norm> = Vec::new();
+        let mut positions: Vec<Vert> = Vec::new();
+        let mut normals: Vec<Norm> = Vec::new();
 
-        for i in vertecis{
-            positions.push(Vert{ position: i.position });
+        for i in vertecis {
+            positions.push(Vert {
+                position: i.position,
+            });
             normals.push(Norm { normal: i.normal });
         }
 
+        let vbp = glium::VertexBuffer::new(display, &positions).unwrap();
+        let vbn = glium::VertexBuffer::new(display, &normals).unwrap();
+        let ixb = glium::IndexBuffer::new(
+            display,
+            glium::index::PrimitiveType::TrianglesList,
+            &indices,
+        )
+        .unwrap();
 
-        let vbp = glium::VertexBuffer::new(display,&positions).unwrap();
-        let vbn = glium::VertexBuffer::new(display,&normals).unwrap();
-        let ixb = glium::IndexBuffer::new(display, glium::index::PrimitiveType::TrianglesList,
-                                                                  &indices).unwrap();
-
-
-        let rot:cgmath::Matrix4<f32> = cgmath::Matrix4::from_angle_x(cgmath::Rad{ 0: (0.0) });
-        let trn:cgmath::Matrix4<f32> = cgmath::Matrix4::from_translation(cgmath::Vector3{
+        let rot: cgmath::Matrix4<f32> = cgmath::Matrix4::from_angle_x(cgmath::Rad { 0: (0.0) });
+        let trn: cgmath::Matrix4<f32> = cgmath::Matrix4::from_translation(cgmath::Vector3 {
             x: (0.0),
             y: (0.0),
-            z: ( 2.0)});
+            z: (2.0),
+        });
 
-        let scl:cgmath::Matrix4<f32> = cgmath::Matrix4::from_scale(0.01);
-
+        let scl: cgmath::Matrix4<f32> = cgmath::Matrix4::from_scale(0.01);
 
         StaticMesh {
             vertices: vbp,
@@ -67,7 +68,7 @@ impl StaticMesh {
             indices: ixb,
             scale: scl,
             rotation: rot,
-            translation: trn
+            translation: trn,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,5 @@ pub mod my_game;
 
 fn main(){
     let mut dummy_game = my_game::DummyGame::new("myGame");
-
     dummy_game.start();
-    
-
 }
